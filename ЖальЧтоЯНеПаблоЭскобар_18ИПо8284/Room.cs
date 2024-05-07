359bc3b422d25d91197568587cf93bdc9bf151da
JakFresko798 <pomidor.pozharov@gmail.com>
2024-05-07 16:06:03 +0300

    Add files via upload

---
+++ ЖальЧтоЯНеПаблоЭскобар_18ИПо8284/Room.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЖальЧтоЯНеПаблоЭскобар_18ИПо8284
+{
+    public class Room
+    {
+        double roomLength;
+        double roomWidth;
+
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return roomWidth; }
+            set { roomWidth= value; }
+        }
+        /// <summary>
+        /// Метод вычисляет периметр комнаты
+        /// </summary>
+        /// <returns>Возвращает значение периметра</returns>
+        public double RoomPerimeter()
+        {
+            return 2 * (roomLength + roomWidth);
+        }
+       /// <summary>
+       /// Метод Вычисляет площадь комнаты
+       /// </summary>
+       /// <returns>Возвращает значение площади</returns>
+        public double RoomArea()
+        {
+            return roomLength * roomWidth;
+        }
+        /// <summary>
+        /// Метод вычисляет число квадратных метров на одного человека
+        /// </summary>
+        /// <param name="np">Число человек</param>
+        /// <returns>возвращает число квадратных метров</returns>
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+        }
+    }
+}
